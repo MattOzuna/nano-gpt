439b0ce38185b52db2738caa5c6b0ea3bba6d646
MattOz <m.ozuna22@gmail.com>
2024-08-19 14:57:34 -0400

    completed video got 1.96 for loss

--- intro-video.py
+++ intro-video.py
@@ -2,23 +2,29 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-torch.manual_seed(1337)
 # ===================================================================================================#
 # hyperparamters
-batch_size = 32  # how many independent sequences will we process in parallel?
-block_size = 8  # what is the maximum context length for predictions?
-n_embd = 32
-max_iters = 3000  # training cycles
-eval_interval = 300  # how many chars generated when sampling
-learning_rate = 1e-2
+batch_size = 64  # how many independent sequences will we process in parallel?
+block_size = 256  # what is the maximum context length for predictions?
+max_iters = 5000  # training cycles
+eval_interval = 500  # how many chars generated when sampling
+learning_rate = 3e-4
+
 device = (
     "cuda"
     if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available() else "cpu"
 )
 print(f"using {device} device")
+
 eval_iters = 200  # how often you print the loss
-dropout = 0.0
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+
+torch.manual_seed(1337)
+
 # ===================================================================================================#
 # read data and sort data
 
@@ -135,10 +141,9 @@ class BigramLanguageModel(nn.Module):
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.blocks = nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
+            *[Block(n_embd, n_head=n_head) for _ in range(n_layer)]
         )
+        self.ln_f = LayerNorm1d(n_embd)  # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -211,7 +216,7 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
 
-        # self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -222,7 +227,7 @@ class Head(nn.Module):
         #  makes sure future doesn't communicate with the past
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T),
         wei = F.softmax(wei, dim=-1)  # (B, T, T)
-        # wei = self.dropout(wei)
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         v = self.value(x)  # (B,T,C)
         out = wei @ v  # (B, T, T) @ (B, T, C) -> (B, T, C)
@@ -237,13 +242,13 @@ class MultiHeadAttention(nn.Module):
         # create multiple heads
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embd, n_embd)
-        # self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         # run all of them in parrallel and concat into a single list on the channel (C) dimension
         out = torch.cat([h(x) for h in self.heads], dim=-1)
         out = self.proj(out)
-        # out = self.dropout(self.proj(out))
+        out = self.dropout(self.proj(out))
         return out
 
 
@@ -260,7 +265,7 @@ class FeedFoward(nn.Module):
             nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
             nn.Linear(4 * n_embd, n_embd),
-            # nn.Dropout(dropout),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -276,15 +281,35 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedFoward(n_embd)
-        # self.ln1 = nn.LayerNorm(n_embd)
-        # self.ln2 = nn.LayerNorm(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
-        # x = x + self.sa(self.ln1(x))
-        # x = x + self.ffwd(self.ln2(x))
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
         return x
+
+
+class LayerNorm1d:
+    # used to be BatchNorm1d
+    # Now normalizes across the rows insted of the columns
+    # We use this normalization before out self attatntion layer and feed forward
+
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+
+    def __call__(self, x):
+        # calculate the forward pass
+        xmean = x.mean(1, keepdim=True)  # batch mean
+        xvar = x.var(1, keepdim=True)  # batch variance
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)  # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
 
 
 # ===================================================================================================#
